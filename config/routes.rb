cb92112b51ded2bb80e81df7a90decea079801b6
mervinhowel <howelgundaya@yahoo.com>
2025-06-18 15:02:23 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,44 @@
 Rails.application.routes.draw do
-  get "user/profile"
-  # API routes
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'signup'
+  }, controllers: {
+    sessions: 'sessions',
+    registrations: 'registrations'
+  }
+
+  # Custom profile route
+  get '/u/:id', to: 'users#profile', as: 'user'
+
+  # Static pages
+  get 'about', to: 'pages#about'
+  get 'up', to: 'rails/health#show', as: :rails_health_check
+
+  # Posts and other resources
+  resources :posts
+  resources :credit_cards, except: [:show, :new]
+
+  # API Routes (JSON)
   scope '/api', defaults: { format: :json } do
     scope '/v1' do
-      devise_for :users, 
+      devise_for :users,
+        path: '',
         path_names: { sign_in: 'login', sign_out: 'logout' },
-        controllers: { registrations: 'registrations', sessions: 'sessions' },
+        controllers: {
+          registrations: 'registrations',
+          sessions: 'sessions'
+        },
         as: :api_v1
 
       devise_scope :user do
         get 'users/current', to: 'sessions#show'
       end
 
-      resources :pages, only: %i[index]
+      resources :pages, only: [:index]
     end
   end
 
-  # trial
-
-  # Web routes using TurboDeviseController
-  devise_for :users, controllers: { sessions: 'sessions', registrations: 'registrations' }
-  
-  get '/u/:id', to: 'users#profile', as: 'user'
-
-  resources :posts
-  resources :credit_cards, except: [:show, :new]
-
-  get "about", to: "pages#about"
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  root "pages#home"
+  # Root path
+  root 'pages#home'
 end
